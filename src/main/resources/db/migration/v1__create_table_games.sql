749b42a7f2baeced541fcf4a4b12523cdcf10175
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-01-16 10:49:09 -0300

    chore: intial project

---
+++ src/main/resources/db/migration/v1__create_table_games.sql
@@ -0,0 +1,5 @@
+CREATE TABLE games (
+    id serial PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description text
+);
---
+++ src/test/java/com/github/arthurscarpin/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arthurscarpin.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
